f9594bcb80a1e568f78db1d70690e2cf7444d1c2
Rajeev <rajgupta123@gmail.com>
2022-11-01 17:02:25 +0530

    second commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RajPro1.Models;
+
+namespace RajPro1.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AccountController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+      
+    }
+}
