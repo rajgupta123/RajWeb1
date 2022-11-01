1f6cb938084e7817c33f147d6a71155bd562ad28
Rajeev <rajgupta123@gmail.com>
2022-11-01 16:51:44 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RajPro1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
